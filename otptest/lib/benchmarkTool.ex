17b7116172700b09b98d4c6cd65fa798f80c5dd2
liyigenius@gmail.com <Genius336>
2022-10-05 21:48:53 +0800

    new

--- otptest/lib/benchmarkTool.ex
+++ otptest/lib/benchmarkTool.ex
@@ -1,3 +1,34 @@
+defmodule OtpCounter do
+  use GenServer
+
+    def init(state) do
+      {:ok, state}
+    end
+
+    def handle_call({:add, value}, _from, state) do
+      {:reply, state+value, state+value}
+    end
+
+    def handle_call({:get, dummy},_from, state) do
+      {:reply, state, state}
+    end
+
+    #client call
+    def start_link(state \\ 0 ) do
+      GenServer.start_link(__MODULE__, state, name: __MODULE__)
+    end
+
+    def get(dummy \\ 0) do
+      GenServer.call(__MODULE__, {:get, dummy})
+    end
+
+    def add(value) do
+      GenServer.call(__MODULE__, {:add, value})
+    end
+
+end
+
+
 defmodule BenchmarkTool do
 
 
@@ -16,10 +47,19 @@ defmodule BenchmarkTool do
 
 
   def runBench002(n) do
+    OtpCounter.start_link(0)
+    IO.inspect(OtpCounter.get())
     times1 = div(n, 8)
     ll1 = getRange(1, n, times1)
     tasklist = ll1 |> Enum.map( fn(x) -> Task.async(BenchmarkTool, :getPrimeCount, [elem(x,0), elem(x,1)]) end )
-    tasklist |> Enum.each(fn (x) ->   IO.inspect(Task.await(x, 100000) ) end )
+    tasklist |> Enum.each(fn (x) ->  collectRes(x) end  )
+    OtpCounter.get()
+  end
+
+  def collectRes(task1) do
+    res = Task.await(task1, 100000000)
+    OtpCounter.add(res)
+    #IO.inspect(res)
   end
 
   def getRange(offset, total, times) do
@@ -46,6 +86,11 @@ defmodule BenchmarkTool do
 end
 # a = BenchmarkTool.runBench001(100)
 # a |> Enum.each(fn (x) ->  Task.await(x, 100000) end )
-res = BenchmarkTool.runBench002(1000000)
+res = BenchmarkTool.runBench002(10000000)
 IO.inspect(res)
-# IO.inspect(BenchmarkTool.getPrimeCount(1,1000))
+#IO.inspect(BenchmarkTool.getPrimeCount(1,100000))
+# OtpCounter.start_link(0)
+# OtpCounter.add(10)
+# OtpCounter.add(10)
+# res = OtpCounter.get()
+# IO.inspect(res)
