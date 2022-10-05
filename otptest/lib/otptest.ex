51e3332039f4b4611c4da3691475e003155cbe6c
liyigenius@gmail.com <Genius336>
2022-10-05 14:52:31 +0800

    otp test

--- otptest/lib/otptest.ex
+++ otptest/lib/otptest.ex
@@ -16,3 +16,39 @@ defmodule Otptest do
     :world
   end
 end
+
+
+defmodule OtpCache do
+  use GenServer
+
+    def init(state) do
+      {:ok, state}
+    end
+
+    def handle_call({:get, key}, _from, state) do
+      {:reply, Map.get(state, key), state}
+    end
+
+    def handle_cast({:put, key, value}, state) do
+      {:noreply, Map.put(state, key, value)}
+    end
+
+    #client call
+    def start_link(state \\ Map.new ) do
+      GenServer.start_link(__MODULE__, state, name: __MODULE__)
+    end
+
+    def get(key) do
+      GenServer.call(__MODULE__, {:get, key})
+    end
+
+    def put(key, value) do
+      GenServer.cast(__MODULE__, {:put, key, value})
+    end
+
+end
+
+OtpCache.start_link()
+OtpCache.put("key", "value")
+res = OtpCache.get("key")
+IO.inspect(res)
